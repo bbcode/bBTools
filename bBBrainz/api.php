662a5d4c7cda8b12a235a5dc028ef86692d03a43
ThePickleMan <admin.aegamesi@gmail.com>
2012-08-26 01:52:54 +0200

    really phos? added config.php

--- bBBrainz/api.php
+++ bBBrainz/api.php
@@ -1,10 +1,11 @@
 <?php
+require_once('../config.php');
 header('Content-Type: application/javascript; charset=utf-8');
-header('Access-Control-Allow-Origin: https://baconbits.org/');
+header('Access-Control-Allow-Origin: https://'.$site_name_this_is_stupid_nobody_cares.'/');
 require_once('common.php');
 
 if(empty($_GET['artist']) || empty($_GET['album']))
-	die('artist, album, and callback required');
+	die('artist, album required');
 
 $results = search_mb_release($_GET['artist'], $_GET['album']);
 $mbid = $results[0]['id'];
---
+++ config.example.php
@@ -0,0 +1,3 @@
+<?php
+$site_name_this_is_stupid_nobody_cares = "example.org";
+?>
